4b48bf634a97abca931cf467289cc18581c9cebc
Gaurav Srivastava <srivastavaga@vmware.com>
2019-08-28 15:25:46 +0530

    added scss support

--- webpack-client.config.js
+++ webpack-client.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDevelopment = process.env.NODE_ENV === 'development';
 const config = {
     entry: ['./lib/renderer/ClientRenderer.js'],
     output: {
@@ -17,8 +18,49 @@ const config = {
               presets: ["@babel/env", "@babel/react"]
             }
           }
+        },
+        {
+          test: /\.module\.s(a|c)ss$/,
+          loader: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+                camelCase: true,
+                sourceMap: isDevelopment
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
+        },
+        {
+          test: /\.s(a|c)ss$/,
+          exclude: /\.module.(s(a|c)ss)$/,
+          loader: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
         }]
-      }
+      },
+      plugins: [
+        new MiniCssExtractPlugin({
+          filename: isDevelopment ? 'bundle.css' : 'bundle.css',
+          chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+        })
+      ]
 };
 
 module.exports = config;
